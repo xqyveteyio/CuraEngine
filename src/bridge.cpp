ec6474e405ad5387d57b83f6ba55457887d16ee6
keyboard <xqyveteyio@outlook.com>
2026-07-15 15:12:05 +0800

    Update bridge.cpp

--- src/bridge.cpp
+++ src/bridge.cpp
@@ -19,7 +19,6 @@ double bridgeAngle(
     const SliceDataStorage& storage,
     const unsigned layer_nr,
     const unsigned bridge_layer,
-
     const SupportLayer* support_layer,
     Shape& supported_regions)
 {
